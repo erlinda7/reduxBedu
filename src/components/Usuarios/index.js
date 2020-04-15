82ddcb5a27b25b6dc92a0bf0fbe8cdf8f1d62ca6
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-15 18:59:51 -0400

    conexion a un componente

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react'; //import Component
 import axios from 'axios';
 
+import { connect } from 'react-redux';//para poder conectar nuestro reducer al componente
 
 //cambiar const por class
 class Usuarios extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      usuarios: []
-    }
-  }
 
-  async componentDidMount() {
+  // async componentDidMount() {
 
-    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+  //   const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
 
 
-    this.setState({
-      usuarios: respuesta.data
-    })
-  }
+  //   this.setState({
+  //     usuarios: respuesta.data
+  //   })
+  // }
 
   //ya no es necesario const
   ponerFilas = () => (
-    this.state.usuarios.map(usuario => (
+    this.props.usuarios.map(usuario => (
       <tr key={usuario.id}>
         <td>{usuario.name}</td>
         <td>{usuario.email}</td>
@@ -35,6 +30,7 @@ class Usuarios extends Component {
   );
   //necesito poner dentro de un render
   render() {
+    
     return (
       <div>
         <table className="tabla">
@@ -57,5 +53,19 @@ class Usuarios extends Component {
   }
 }
 
+//por default lo entrego todos los reducers
+const mapStateToProps = (reducers) => {
+  //solo quiero usuarios
+  return reducers.usuariosReducer;
+}
 
-export default Usuarios;
+
+//conectando al almacenamiento global
+export default connect(mapStateToProps,{/*Actions */} )(Usuarios);
+//parametros que debe recibir el connect
+  //1ro: 
+      //todos los reducers que el proveedor le va entregar al usuario,
+      // yo voy poder escoger cual de estos necesito(1, 2 o todos) 
+      //entregarlselo a mi componente que el componenente lo pueda utilizar
+  //2do:
+    //se lo envio todos las acciones  o "Actions Creators"
