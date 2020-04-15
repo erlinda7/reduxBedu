3b89b0105e2e466f22f7c18b740abef02b6133e8
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-15 18:31:59 -0400

    creando un reducer valido y enviandolo a reducers para que lo combine

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 //vamos a combinar todos los reducers que necesitamos para mandarcelo a nuestra aplicacion
 import {combineReducers} from 'redux';
+import usuariosReducer from './usuariosReducer'; //impotamos el reducir creado
+
 
 export default combineReducers({
-    
+    usuariosReducer //y se lo damos para que lo este combinando
 })
