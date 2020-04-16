efee497b4d79ce736fa26c306ca805808dbea33a
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-15 21:52:00 -0400

    manejando errores de asincronia

--- src/actions/UsuariosActions.js
+++ src/actions/UsuariosActions.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
-import {TRAER_TODOS} from '../types/usuariosTypes'
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes'
 //vamos hacer una accion la cual el componente va llamar y esta accion va entregar algo
 
 //una funcion que va traer otra funcion .. son 2 funciones
 //el dispatch es el que va disparar esa llamada y va contactar al reducer para que todo este cambio de estado
 
-export const  traerTodos = () => async (dispatch)=>{
-    
-    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+export const traerTodos = () => async (dispatch) => {
 
-    dispatch ({
-        type : TRAER_TODOS,  //en usuariosReducer hay un caso que es 'traer_usuarios' a este es a quien quiero llamar
-        payload: respuesta.data
+    dispatch({
+        type: CARGANDO
     })
+
+    try {
+        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+        dispatch({
+            type: TRAER_TODOS,  //en usuariosReducer hay un caso que es 'traer_usuarios' a este es a quien quiero llamar
+            payload: respuesta.data
+        })
+    } catch (error) {
+        console.log('Error:', error.message);
+        dispatch({
+            type: ERROR,
+            payload: error.message
+        }
+        )
+    }
 }
--- src/types/usuariosTypes.js
+++ src/types/usuariosTypes.js
@@ -1,7 +1,9 @@
 //todo lo que voy mandar del actions al reducer 
 //y del reducir al componente lo voy a traer desde aqui
 
-export const TRAER_TODOS ='traer_todos'
+export const TRAER_TODOS = 'traer_todos';
+export const CARGANDO = 'cargando';
+export const ERROR = 'error'
 
 //si el case en reducir es case es traer_todos y en actions trear_todos no muestra nada en pantallla ni resultado 
 //ni error y es muy dificil encontrarlo por lo cual esto te ayuda mucho usar variables en types
