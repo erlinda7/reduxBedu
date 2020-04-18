a753c97365ff25e6a424e19962fc6de8ea5ce49e
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-18 09:42:16 -0400

    evitar sobreescritura

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,33 +1,74 @@
-import  React, {Component } from "react";
+import React, { Component } from "react";
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import Spinner from '../general/Spinner';
+import Fatal from '../general/Fatal';
+
 import * as usuariosActions from '../../actions/UsuariosActions';
 import * as publicacionesActions from '../../actions/publicacionesActions';
 
-const {traerTodos: usuariosTraerTodos} = usuariosActions;
-const {traerPorUsuario: publicacionesTraerPorUsuario} = publicacionesActions;
+const { traerTodos: usuariosTraerTodos } = usuariosActions;
+const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 
-export class Publicaciones extends Component{
-    async componentDidMount(){
-        if(!this.props.usuariosReducer.usuarios.length){ //sino tienes usuarios
-            await this.props.usuariosTraerTodos()
+export class Publicaciones extends Component {
+    async componentDidMount() {
+        const {
+            usuariosTraerTodos,
+            publicacionesTraerPorUsuario,
+            match: { params: { key } }
+        } = this.props;
+
+        //usuariosREducir no lo puedo destructurar porque cambia el estado y compDidMount solo se ejecuta una vez
+        if (!this.props.usuariosReducer.usuarios.length) { //sino tienes usuarios
+            await usuariosTraerTodos()
         }
-        this.props.publicacionesTraerPorUsuario(this.props.match.params.key);
+
+        if(this.props.usuariosReducer.error){
+            return ;
+        }
+
+        //si publicaciones_key ya esta no lo traigas
+        if (!('publicaciones_key' in this.props.usuariosReducer.usuarios[key])) {
+            publicacionesTraerPorUsuario(key);
+        }
+
     }
 
-    render(){
+    ponerUsuario = () => {
+        const {
+            usuariosReducer,
+            match: { params: { key } }
+        } = this.props;
+
+        if(usuariosReducer.error){
+            return <Fatal mensaje= {usuariosReducer.error}/>
+        }
+
+        if(!usuariosReducer.usuarios.length || usuariosReducer.cargando){
+            return <Spinner/>
+        }
+
+        const nombre = usuariosReducer.usuarios[key].name;
+
+        return(
+        <h1>Publicaciones de {nombre}</h1>
+        )
+    };
+
+    render() {
         console.log(this.props);
-        
+
         return (
             <div>
-                <h1>Publicaciones de </h1>
+                
                 {this.props.match.params.key}
+                {this.ponerUsuario()}
             </div>
         )
     }
 }
 
-const mapStateToProps = ({usuariosReducer, publicacionesReducer}) =>{ //desestructurando
+const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => { //desestructurando
     return {
         usuariosReducer,
         publicacionesReducer
@@ -39,7 +80,7 @@ const mapDispatchToProps = {
     //...usuariosActions,  //me va entregar todo lo de publicacion accion
     //...publicacionesActions  //en ambas acciones tenemos un metodo traerTodos() , entonces agarra este ultimo y trae eso
 
-        usuariosTraerTodos,                    //para que funcione bien traemos la des estructaramos y la renombramos
-        publicacionesTraerPorUsuario
+    usuariosTraerTodos,                    //para que funcione bien traemos la des estructaramos y la renombramos
+    publicacionesTraerPorUsuario
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
