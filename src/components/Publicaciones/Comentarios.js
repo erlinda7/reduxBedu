cfed3564a087786b2d7cbb3a2d542b225505e1f7
Erlinda <erlindachambimanzanoerlinda@gamil.com>
2020-04-18 15:03:28 -0400

    state compartido

--- src/components/Publicaciones/Comentarios.js
+++ src/components/Publicaciones/Comentarios.js
@@ -1,35 +1,35 @@
 import React from 'react';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import Spinner from '../general/Spinner';
 import Fatal from '../general/Fatal';
 
-const Comentarios = (props) =>{
-    if(props.cargando){
-        return <Spinner/>
+const Comentarios = (props) => {
+    if (props.com_error) {
+        return <Fatal mensaje={props.com_error} />
     }
 
-    if(props.error){
-        return <Fatal mensaje ={props.error}/>
+    if (props.com_cargando && !props.comentarios.length) {
+        return <Spinner />
     }
 
-    const ponerComentarios = ()=>(
-        props.comentarios.map((comentario)=>(
+    const ponerComentarios = () => (
+        props.comentarios.map((comentario) => (
             <li>
                 <b><u>{comentario.email}</u></b>
-                <br/>
+                <br />
                 {comentario.body}
             </li>
         ))
     )
-    return(
+    return (
         <ul>
-           {ponerComentarios()}
+            {ponerComentarios()}
         </ul>
     )
 }
 //2da forma conectar con redux
-const mapStateToProps= ({publicacionesReducer})=>publicacionesReducer;
+const mapStateToProps = ({ publicacionesReducer }) => publicacionesReducer;
 
 export default connect(mapStateToProps)(Comentarios);//ya con eso tengo en el props si esta cargando o error
